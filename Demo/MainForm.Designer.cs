b969499938032e68de77731a63dd543cfbbc83ec
i818292 <ray.kuo@sap.com>
2014-03-12 13:49:26 -0700

    add demo project

---
+++ Demo/MainForm.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace MultiFaceRec
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trainButton = new System.Windows.Forms.Button();
+            this.personToTrainText = new System.Windows.Forms.TextBox();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.loggerLabel = new System.Windows.Forms.Label();
+            this.indicatorLabel = new System.Windows.Forms.Label();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // trainButton
+            // 
+            this.trainButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.trainButton.Location = new System.Drawing.Point(444, 372);
+            this.trainButton.Name = "trainButton";
+            this.trainButton.Size = new System.Drawing.Size(156, 29);
+            this.trainButton.TabIndex = 3;
+            this.trainButton.Text = "Train";
+            this.trainButton.UseVisualStyleBackColor = true;
+            this.trainButton.Click += new System.EventHandler(this.trainButton_Click);
+            // 
+            // personToTrainText
+            // 
+            this.personToTrainText.AccessibleDescription = "Enter the name";
+            this.personToTrainText.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.personToTrainText.Location = new System.Drawing.Point(242, 372);
+            this.personToTrainText.Name = "personToTrainText";
+            this.personToTrainText.Size = new System.Drawing.Size(156, 29);
+            this.personToTrainText.TabIndex = 7;
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.Font = new System.Drawing.Font("Verdana", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameLabel.ForeColor = System.Drawing.Color.Black;
+            this.nameLabel.Location = new System.Drawing.Point(45, 61);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(752, 154);
+            this.nameLabel.TabIndex = 16;
+            this.nameLabel.Text = "Nobody";
+            this.nameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // loggerLabel
+            // 
+            this.loggerLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.loggerLabel.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loggerLabel.ForeColor = System.Drawing.Color.Blue;
+            this.loggerLabel.Location = new System.Drawing.Point(242, 234);
+            this.loggerLabel.Name = "loggerLabel";
+            this.loggerLabel.Size = new System.Drawing.Size(358, 121);
+            this.loggerLabel.TabIndex = 17;
+            // 
+            // indicatorLabel
+            // 
+            this.indicatorLabel.BackColor = System.Drawing.Color.Red;
+            this.indicatorLabel.Location = new System.Drawing.Point(399, 21);
+            this.indicatorLabel.Name = "indicatorLabel";
+            this.indicatorLabel.Size = new System.Drawing.Size(44, 40);
+            this.indicatorLabel.TabIndex = 18;
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeLabel.Location = new System.Drawing.Point(625, 236);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(100, 23);
+            this.timeLabel.TabIndex = 19;
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(837, 436);
+            this.Controls.Add(this.timeLabel);
+            this.Controls.Add(this.indicatorLabel);
+            this.Controls.Add(this.loggerLabel);
+            this.Controls.Add(this.personToTrainText);
+            this.Controls.Add(this.trainButton);
+            this.Controls.Add(this.nameLabel);
+            this.Name = "FrmPrincipal";
+            this.Text = "Facial Recognizer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button trainButton;
+        private System.Windows.Forms.TextBox personToTrainText;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label loggerLabel;
+        private System.Windows.Forms.Label indicatorLabel;
+        private System.Windows.Forms.Label timeLabel;
+    }
+}
+
---
+++ Demo/MainForm.cs
@@ -0,0 +1,131 @@
+﻿
+//Multiple face detection and recognition in real time
+//Using EmguCV cross platform .Net wrapper to the Intel OpenCV image processing library for C#.Net
+//Writed by Sergio Andrés Guitérrez Rojas
+//"Serg3ant" for the delveloper comunity
+// Sergiogut1805@hotmail.com
+//Regards from Bucaramanga-Colombia ;)
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+using System.IO;
+using System.Diagnostics;
+using System.Timers;
+using System.Threading;
+
+using FaceRecognizer;
+
+namespace MultiFaceRec
+{
+    public partial class FrmPrincipal : Form
+    {
+        private Detector detector = null;
+        private DateTime startTime;
+        private Thread thread = null;
+
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+
+            detector = new Detector(Application.StartupPath);
+            detector.match += new Detector.MatchHandler(handleMatched);
+            detector.logger += new Detector.LogHandler(detector_logger);
+            detector.trainComplete += new Detector.TrainCompleteHandler(detector_trainComplete);
+            this.FormClosed += new FormClosedEventHandler(FrmPrincipal_FormClosed);            
+
+            //Application.Idle += new EventHandler(idle);
+            thread = new Thread(new ThreadStart(run));
+            thread.Start();
+        }
+
+        //public void capture()//(object sender, EventArgs e)
+        void idle(object sender, EventArgs e)
+        {
+            this.detector.capture();
+        }
+
+        void run()
+        {
+            while (true)
+            {
+                this.detector.capture();
+            }
+        }
+
+        void FrmPrincipal_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            //Application.Idle -= new EventHandler(detector.capture);
+            this.thread.Abort();
+        }
+
+        void detector_logger(string message)
+        {
+            this.Invoke((MethodInvoker)delegate
+            {
+                this.loggerLabel.Text = message;
+            });
+        }
+
+        private void handleMatched(string match, int count)
+        {
+            this.Invoke((MethodInvoker)delegate
+            {
+                if (count == -2)
+                {
+                    this.indicatorLabel.BackColor = Color.Gray;
+                    this.loggerLabel.Text = "";
+                }
+                else if (count == 0)
+                {
+                    this.indicatorLabel.BackColor = Color.Red;
+                }
+                else if (count > 0 && count < 10)
+                {
+                    if (count == 1)
+                    {
+                        this.startTime = DateTime.Now;
+                    }
+                    else if (count == 9)
+                    {
+                        this.timeLabel.Text = DateTime.Now.Subtract(this.startTime).TotalSeconds.ToString();
+                    }
+                    this.indicatorLabel.BackColor = Color.Yellow;
+                }
+                else
+                {
+                    this.indicatorLabel.BackColor = Color.Green;
+
+                }
+
+                if (String.IsNullOrEmpty(match) == false)
+                {
+                    match = String.Format("Hi, {0}", match);
+                }
+                this.nameLabel.Text = match;
+            });
+        }
+
+        private void trainButton_Click(object sender, System.EventArgs e)
+        {
+            this.detector.startTraining(this.personToTrainText.Text);
+            this.trainButton.Enabled = false;
+            this.trainButton.Text = "Training...";
+            this.loggerLabel.Text = "";
+        }
+
+        void detector_trainComplete()
+        {
+            this.Invoke((MethodInvoker)delegate
+            {
+                this.personToTrainText.Text = "";
+                this.trainButton.Enabled = true;
+                this.trainButton.Text = "Train";
+            });
+        }
+    }
+}
